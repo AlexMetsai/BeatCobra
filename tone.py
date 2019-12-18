6d435fd54338be59638e9c66be7ffcf415b1881a
Alexandros Metsai <alexmetsai@gmail.com>
2019-12-18 17:26:11 +0200

    Update tone.py

--- tone.py
+++ tone.py
@@ -9,6 +9,8 @@ class ToneGenerator:
   Parent class for creating combined waveforms.
   Duration in seconds (s).
   '''
-  def __init__():
+  def __init__(self, duration=3, rate=22050, harmonics_num=3):
+    # TODO
   
-  def generate():
+  def generate(self, f):
+    # TODO
