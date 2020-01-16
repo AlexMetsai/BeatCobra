563f6e805b071dfc153256e9bec5513d4ea583b6
Alexandros Metsai <alexmetsai@gmail.com>
2020-01-16 12:37:49 +0200

    PercussionGenerator constructor

--- percussion.py
+++ percussion.py
@@ -9,3 +9,7 @@ class PercussionGenerator:
   Parent class for creating percussion sounds.
   Duration in seconds (s).
   '''
+  def __init__(self, duration=3, rate=22050):
+    self.T = duration
+    self.rate = rate
+    self.decay_factor = decay_factor
