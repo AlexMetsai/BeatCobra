f8c139df67faeefdf2d7102b2938e229ef4f669e
Alexandros Metsai <alexmetsai@gmail.com>
2020-01-17 11:14:42 +0200

    minor fix in PercussionGenerator

--- percussion.py
+++ percussion.py
@@ -4,12 +4,13 @@ import numpy as np
 import wavio
 from scipy import signal
 
+
 class PercussionGenerator:
   '''
   Parent class for creating percussion sounds.
   Duration in seconds (s).
   '''
-  def __init__(self, duration=3, rate=22050):
+  def __init__(self, duration=3, rate=22050, decay_factor=10):
     self.T = duration
     self.rate = rate
     self.decay_factor = decay_factor
