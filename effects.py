bfd83508b5238c4e50832be35bb6450f08dd6602
Alexandros Metsai <alexmetsai@gmail.com>
2020-02-04 13:34:14 +0200

    Update effects.py

--- effects.py
+++ effects.py
@@ -1,1 +1,5 @@
 # Some effects should be included, like Reverb and Delay.
+
+def Reverb(signal):
+    # How to implement this? Would it be better as a class
+    # or as a function? For now we go with a function call.
