c0846ccecceba5dccb4b2f4d7d6339fa56d19757
Alexandros Metsai <alexmetsai@gmail.com>
2020-02-11 16:38:57 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -42,7 +42,7 @@ def snare_kick_kick(beat_repeats=30):
     # Write output to file.
     wavio.write('generated_demos/snare_kick_kick_beat.wav', beat, snare.rate, sampwidth=SAMPWIDTH)
 
-def major_scale(root_note=440, waveform='square', mode='fixed'):
+def major_scale(A4=440, waveform='square', mode='fixed'):
     '''
     Simple demo playing the major scale, given the root note.
     '''
