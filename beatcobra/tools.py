7a50bb5cc8a3ab92bd5c93f9685753dfe403673d
Alexandros Metsai <alexmetsai@gmail.com>
2020-02-11 16:51:11 +0200

    define frequency-key milestone

--- beatcobra/tools.py
+++ beatcobra/tools.py
@@ -1,1 +1,8 @@
 
+def return_key_frequency(A4=440):
+    # Things that need to be implemented for now are:
+    # 1. Map notes to keys, e.g. A4 = 49th key, etc.
+    # 2. Compute the frequency of its key.
+    # The second can be easily achived by the following equation:
+    # f(n) = (12th-root(2))^(n-49) * 440Hz
+    # https://en.wikipedia.org/wiki/Piano_key_frequencies
