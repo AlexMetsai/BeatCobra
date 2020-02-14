8cc57cd32b5d2e6c5249a022dc328c5b6ca1e8d4
Alexandros Metsai <alexmetsai@gmail.com>
2020-02-14 16:28:23 +0200

    further enhancements for the future

--- beatcobra/effects.py
+++ beatcobra/effects.py
@@ -1,8 +1,11 @@
 # Some effects should be included, like Reverb and Delay.
 
 def Reverb(signal):
-    # How to implement this? Would it be better as a class
-    # or as a function? For now we go with a function call.
+    # How to implement this? Would it be better as a class or as a function? 
+	# For now we go with a function call.
     #
-    # A simple protype reverb effect could be implemented
-    # using all-pass filters.
+    # A simple protype reverb effect could be implemented using all-pass filters.
+    #
+    # Furthermore, we can utilize comb filters for a basic sound, and then 
+	# enhance the effect with all-pass filters. We can show the incremental
+	# addition of filters and demonstrate each component's contribution.
