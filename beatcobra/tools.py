95202e9cf18f32852af494b8e4db7aefc06eaab7
Alexandros Metsai <alexmetsai@gmail.com>
2020-03-05 18:29:53 +0200

    add sort description

--- beatcobra/tools.py
+++ beatcobra/tools.py
@@ -1,11 +1,7 @@
 import json
 
 def return_key_frequency(key='C4', A4=440):
-    # Things that need to be implemented for now are:
-    # 1. Map notes to keys, e.g. A4 = 49th key, etc.
-    # 2. Compute the frequency of its key.
-    # The second can be easily achived by the following equation:
-    # f(n) = (12th-root(2))^(n-49) * 440Hz
+    # Return the frequency of the provided note/piano key.
     # https://en.wikipedia.org/wiki/Piano_key_frequencies
     
     # Pseudocode:
