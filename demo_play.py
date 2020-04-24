8dbc550b2f2b65db0236db44abf27f1184db0707
Alexandros Metsai <alexmetsai@gmail.com>
2020-04-24 14:24:53 +0300

    Update demo_play.py

--- demo_play.py
+++ demo_play.py
@@ -1,2 +1,23 @@
 # A simple demo with current features of BeatCobra.
 # Save generated audio as as wav/mp3 files.
+import time
+
+if __name__=='__main__':
+    
+    print("You may have have to adjust the volume depending on your device.")
+    print("The demo will begin in 5 seconds.")
+    print("MIND YOUR EARS IF WEARING HEADPHONES!")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    print("MIND YOUR EARS IF WEARING HEADPHONES!")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    
+    # play audio
+    
