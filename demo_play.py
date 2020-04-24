0dec442e1cdc42c0fa299dbd13a288f9bf8ebaa4
Alexandros Metsai <alexmetsai@gmail.com>
2020-04-24 14:28:06 +0300

    major scale demo

--- demo_play.py
+++ demo_play.py
@@ -2,6 +2,32 @@
 # Save generated audio as as wav/mp3 files.
 import time
 
+def major_scale(A4=440, waveform='square', mode='fixed'):
+    '''
+    Simple demo playing the major scale, given the root note.
+    '''
+    # Should I calculate the frequencies or use standard values? 
+    if mode=='fixed':
+        C_major = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]
+    else:
+        print('Mode not supported')
+        exit(1)
+    
+    if waveform=='square':
+        generator = SquareGenerator(duration=1)
+    elif waveform=='saw':
+        generator = SawGenerator(duration=1)
+    else:
+        print('Waveform generator not supported.')
+        exit(1)
+    
+    scale = []
+    for frequency in C_major:
+        scale = np.concatenate((scale, generator.generate(frequency)), axis=0)
+    # TODO
+    # play audio
+
+
 if __name__=='__main__':
     
     print("You may have have to adjust the volume depending on your device.")
@@ -20,4 +46,6 @@ if __name__=='__main__':
     print("...")
     
     # play audio
+    print("Demo of the C major scale using a square waveform.")
+    major_scale(waveform='square')
     
