15baf24a48dfbb6f26e215eb9c3223d2bfcad60c
Alexandros Metsai <alexmetsai@gmail.com>
2020-04-24 14:19:06 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,5 @@
-# Make a simple demo with current features of BeatCobra.
-# Add option for playing audio (once or in loop) or for saving
-# as wav/mp3 files.
+# A simple demo with current features of BeatCobra.
+# Save generated audio as as wav/mp3 files.
 
 import wavio
 import numpy as np
