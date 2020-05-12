e32f2fb3da191261c7d440fe91281fe17699358d
Alexandros Metsai <alexmetsai@gmail.com>
2020-05-12 16:59:38 +0300

    freq functionality

--- demo.py
+++ demo.py
@@ -6,6 +6,7 @@ import wavio
 import numpy as np
 from beatcobra.percussion import *
 from beatcobra.tone import *
+from beatcobra.tools import return_key_frequency
 
 # For a 24 bit file, wav.sampwidth must be equal to 3.
 SAMPWIDTH = 3
@@ -46,9 +47,14 @@ def major_scale(A4=440, waveform='square', mode='fixed'):
     '''
     Simple demo playing the major scale, given the root note.
     '''
-    # Should I calculate the frequencies or use standard values? 
+    # You can use fixed frequencies, or calculate the  
+    # corresponding ones for each note/tone.
     if mode=='fixed':
         C_major = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]
+    elif mode=="dynamic":
+        # A tool for calculated a note's frequency is now implemented.
+        # Include this functionality here.
+        pass
     else:
         print('Mode not supported')
         sys.exit()
